f836ff1e703812897f5b61e52d628c0006d35fd1
Elias Peeters <eliaspeeters@Eliass-MacBook-Pro.local>
2018-09-02 20:31:32 +0200

    Alles da

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onscroll = showNavigatorBar;
+
+function showNavigatorBar() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("Navigation").style.backgroundColor = "rgba(34,34, 34, 0.95)";
+    }
+    else {
+        document.getElementById("Navigation").style.backgroundColor ="";
+    }
+}
+
+
+function scroll() {
+    document.body.scrollTop = 800;
+    document.documentElement.scrollTop = 800;
+}
+
+
+
